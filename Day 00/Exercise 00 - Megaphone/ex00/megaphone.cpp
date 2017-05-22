c69384cd3775150ec06ad22673e20ae69c0d7ee8
Matthew Williams <ElCapitan@Matthews-iMac.local>
2017-05-23 00:04:57 +0200

    d00 done

--- Day 00/Exercise 00 - Megaphone/ex00/megaphone.cpp
+++ Day 00/Exercise 00 - Megaphone/ex00/megaphone.cpp
@@ -4,19 +4,12 @@ int     main(int argc, char **argv)
 {
     if (argc > 1)
     {
-        for (int idx = 0; idx < argc; idx++)
-        {
-            for (int cidx = 0; cidx < strlen(argv[idx]); cidx++)
-            {
+        for (int idx = 1; idx < argc; idx++)
+            for (size_t cidx = 0; cidx < strlen(argv[idx]); cidx++)
                 if (argv[idx][cidx] != '\"' && argv[idx][cidx] != '\'')
-                {
-                    std::cout << toupper(argv[idx][cidx]);
-                }
-            }
-        }
+                    std::cout << (char)toupper(argv[idx][cidx]);
+		std::cout << std::endl;
     }
     else
-    {
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    }
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 }
