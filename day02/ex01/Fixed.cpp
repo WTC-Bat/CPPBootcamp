4fbe5284cdcf3ac066c48da2856f48be948ff6a4
FatBat <myshorts.mail@gmail.com>
2017-05-25 01:40:56 +0200

    d02-ex01 done

--- day02/ex01/Fixed.cpp
+++ day02/ex01/Fixed.cpp
@@ -16,8 +16,30 @@ Fixed::Fixed(const Fixed &fixed)
     *this = fixed;
 }
 
+Fixed::Fixed(const int ivalue)
+{
+    std::cout << "Int constructor called" << std::endl;
+
+    this->_pointValue = (ivalue << this->_fractionalBits);
+}
+
+Fixed::Fixed(const float fvalue)
+{
+    std::cout << "Float constructor called" << std::endl;
+
+    this->_pointValue = roundf(fvalue * 256.0);
+}
+
+/* Destructors */
+
+Fixed::~Fixed(void)
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
 /* Operator Overloads */
-Fixed&  Fixed::operator=(const Fixed &fixed)
+
+Fixed&          Fixed::operator=(const Fixed &fixed)
 {
     std::cout << "Assignation operator called" << std::endl;
 
@@ -26,28 +48,35 @@ Fixed&  Fixed::operator=(const Fixed &fixed)
     return (*this);
 }
 
-/* Destructors */
-
-Fixed::~Fixed(void)
+std::ostream&   operator<<(std::ostream& out, const Fixed& fixed)
 {
-    std::cout << "Destructor called" << std::endl;
+    out << fixed.toFloat();
 
-    return ;
+    return (out);
 }
 
 /* Member Functions */
 
-int     Fixed::getRawBits(void) const
+int             Fixed::getRawBits(void) const
 {
     std::cout << "getRawBits member function called" << std::endl;
 
     return (this->_pointValue);
 }
 
-void    Fixed::setRawBits(const int raw)
+void            Fixed::setRawBits(const int raw)
 {
-    //???
     std::cout << "setRawBits member fuction called" << std::endl;
 
     this->_pointValue = raw;
+}
+
+float           Fixed::toFloat(void) const
+{
+    return (this->_pointValue / 256.0);
+}
+
+int             Fixed::toInt(void) const
+{
+    return (this->_pointValue / 256);
 }
--- day02/ex01/Fixed.hpp
+++ day02/ex01/Fixed.hpp
@@ -2,18 +2,21 @@
 # define FIXED_HPP
 
 # include <iostream>
+# include <cmath>
 
 class Fixed
 {
 private:
     /* Fields */
     int                 _pointValue;
-    static const int    _fractionalBits;
+    static const int    _fractionalBits = 8;
 
 public:
     /* Constructors */
     Fixed(void);
     Fixed(const Fixed &fixedClass);
+    Fixed(const int ivalue);
+    Fixed(const float fvalue);
 
     /* Operator Overloads */
     Fixed& operator=(const Fixed& fixed);
@@ -24,6 +27,11 @@ public:
     /* Member Functions */
     int                 getRawBits(void) const;
     void                setRawBits(int const raw);
+    float               toFloat(void) const;
+    int                 toInt(void) const;
 };
+
+/* Non-Members */
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed);
 
 #endif
